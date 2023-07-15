96154971396951c72c4c8ee0f86a027b76f1e69b
Amit Singh <amitds1997@gmail.com>
2023-07-15 21:49:49 +0530

    Add support to parse common used URIs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,20 +1,27 @@
 local wezterm = require('wezterm')
+local hyperlink_rules = require('hyperlink_rules')
 
 local config = {
+  -- General config
   audible_bell = "Disabled",
+  check_for_updates = false,
+  hyperlink_rules = hyperlink_rules,
+  native_macos_fullscreen_mode = true,
+
+  -- Appearance configuration
+  color_scheme = 'Catppuccin Mocha (Gogh)',
   default_cursor_style = "BlinkingUnderline",
   hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
   window_background_opacity = 0.85,
-  color_scheme = 'Catppuccin Mocha (Gogh)',
+
+  -- Font configuration
+  font_size = 16,
   font = wezterm.font_with_fallback {
     { family = "Rec Mono Duotone",                 weight = "Regular" },
     { family = "FantasqueSansMono Nerd Font Mono", weight = "Regular", stretch = "Normal", style = "Normal" },
     'Noto Color Emoji',
   },
-  font_size = 16,
-  native_macos_fullscreen_mode = true,
-  use_fancy_tab_bar = false,
-  check_for_updates = false,
 }
 
 local appearance = wezterm.gui.get_appearance()
--- .config/zsh/conf.d/aliases.zsh
+++ .config/zsh/conf.d/aliases.zsh
@@ -1,18 +1,8 @@
 alias vi="nvim"
 alias vim="nvim"
-alias dirh="dirs -v"
 
 alias bat="bat --theme OneHalfDark"
-alias kitty_ssh="kitty +kitten ssh"
 alias gst="git status"
-
-# Exclude common non-necessary directories from being searched
-GREP_EXCL=(.bzr CVS .git .hg .svn .idea .tox)
-alias grep="${aliases[grep]:-grep} --exclude-dir={\${(j.,.)GREP_EXCL}}"
-
-# Exit aliases
-alias zz="exit"
-alias quit="exit"
 
 # URL decode/encode
 alias urldecode='python3 -c "import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))"'
