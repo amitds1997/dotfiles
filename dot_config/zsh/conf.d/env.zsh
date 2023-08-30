1f49221ca317caa009dd4008b28fa7b048d66b6b
Amit Singh <amitds1997@gmail.com>
2023-08-30 22:30:11 +0530

    Initial commit #5

--- dot_config/zsh/conf.d/env.zsh
+++ dot_config/zsh/conf.d/env.zsh
@@ -4,29 +4,23 @@ export VISUAL=nvim
 # Needed to run git commit correctly with GPG
 export GPG_TTY=$TTY
 
-# Correctly set up JAVA
-if [[ $(uname) == "Darwin" ]]; then
-  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8.0_332)
-fi
-
 # Set correct colors for history substring search
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="bold"
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND="fg=red,bold"
 
 path=(
-  # core
   $HOME/{,s}bin(N)
-  /opt/{homebrew,local}/{,s}bin(N)
+  /opt/local/{,s}bin(N)
   /usr/local/{,s}bin(N)
-
-  # apps
-  $HOMEBREW_PREFIX/opt/curl/bin(N)
-  $HOMEBREW_PREFIX/opt/go/libexec/bin(N)
-  $HOMEBREW_PREFIX/opt/ruby/bin(N)
-  $HOMEBREW_PREFIX/lib/ruby/gems/*/bin(N)
-  $HOME/.gem/ruby/*/bin(N)
-  $HOME/.cargo/bin(N)
-  $HOMEBREW_PREFIX/share/npm/bin(N)
-
   $path
 )
+
+# Correctly set up JAVA
+if [[ $(uname) == "Darwin" ]]; then
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8.0_332)
+  path=(
+    /opt/{homebrew,local}/{,s}bin(N)
+    $path
+  )
+fi
+
