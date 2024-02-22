f5f47256a64b440644cd104b67233ed86adadfc7
Amit Singh <amitds1997@gmail.com>
2024-02-22 13:44:04 +0530

    chore: add neovim configuration

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -43,6 +43,9 @@ local treesitter_config = function()
       end,
       additional_vim_regex_highlighting = false,
     },
+    indent = {
+      enable = true,
+    },
     incremental_selection = {
       enable = true,
     },
